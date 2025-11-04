604ff15d7bfdd217a647068252f60d2dc77c637f
Guanli Liu <liuguanli22@gmail.com>
2025-11-05 10:33:57 +1100

    update run_benchmark

--- scripts/run_benchmark.sh
+++ scripts/run_benchmark.sh
@@ -2,17 +2,17 @@
 
 bash ./scripts/lakehouse_setup.sh --repair
 
-# bash ./scripts/write_tpch.sh
+# bash ./scripts/run_tpch_write.sh
 
-# bash scripts/write_tpch.sh # Overall
-# bash scripts/write_tpch.sh --scales "1 4 16" # 172.26.147.188
-# bash scripts/write_tpch.sh --engines delta,iceberg --scales "64" # 172.26.147.188
-bash scripts/write_tpch.sh --hudi --scales "64"  # 172.26.146.47
+# bash scripts/run_tpch_write.sh # Overall
+# bash scripts/run_tpch_write.sh --scales "1 4 16" # 172.26.147.188
+# bash scripts/run_tpch_write.sh --engines delta,iceberg --scales "64" # 172.26.147.188
+bash scripts/run_tpch_write.sh --hudi --scales "64"  # 172.26.146.47
 RUNNER_ARGS="--hudi" bash run_tpch_query.sh 64
-bash scripts/clean_data.sh --scales "64" --yes
+# bash scripts/clean_data.sh --scales "64" --yes
 
-# bash scripts/write_tpch.sh --engines delta,hudi --scales "1 64"
-# bash scripts/write_tpch.sh --delta --iceberg --scales "1 4 16 64"
+# bash scripts/run_tpch_write.sh --engines delta,hudi --scales "1 64"
+# bash scripts/run_tpch_write.sh --delta --iceberg --scales "1 4 16 64"
 
 
 
